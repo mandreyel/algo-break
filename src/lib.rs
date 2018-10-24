650176a57eaa5d4d556ef408a24cbda238084501
mandreyel <mandreyel@protonmail.com>
2018-10-24 10:32:38 +0200

    Create insertion sort

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+pub mod sort {
+    pub fn insertion_sort<T: PartialOrd>(array: &mut [T]) {
+        let mut i = 1;
+        while i < array.len() {
+            let mut k = i;
+            while k > 0 && array[k - 1] > array[k] {
+                array.swap(k, k - 1);
+                k -= 1;
+            }
+            i += 1;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn insertion_sort() {
+        let mut array = [3, 9, 2, 1];
+        sort::insertion_sort(&mut array);
+        assert_eq!(array, [1, 2, 3, 9]);
     }
 }
